019dd5493e1fe1a6494f8c7772d1e74c01fdd261
Theawesometurtl <shablammmo@gmail.com>
2024-03-05 22:11:47 -0800

    beautiful clean code

--- src/main/main.ino
+++ src/main/main.ino
@@ -5,11 +5,10 @@ int const pin2 = A2;       // Preamp output pin connected to A0
 int const pin3 = A3;       // Preamp output pin connected to A0
 int const pin4 = A4;       // Preamp output pin connected to A0
 
-unsigned int sample0;
-unsigned int sample1;
-unsigned int sample2;
-unsigned int sample3;
-unsigned int sample4;
+int const pins[5] = {pin0, pin1, pin2, pin3, pin4};
+unsigned int sample[5] = {0, 0, 0, 0, 0};
+String myString = "";
+
 
 
 #include <SD.h>
@@ -17,11 +16,6 @@ unsigned int sample4;
 #include<SoftwareSerial.h>  // The library to create a secondary serial monitor on arduino uno.
 
 SoftwareSerial SUART(2, 3); // Sets the input and output ports to Digital Pins 3 and 4. They should be reversed with the pins on the speedometer. 
-char myData[55] = "";       // Creates a blank character array of size 10
-int i = 0;
-int f = 0;
-int z;
-
 
 
 
@@ -33,134 +27,47 @@ void setup()
 
 void loop()
 {
+  // collect data for 50 mS and then plot data
   unsigned long startMillis = millis(); // Start of sample window
-  unsigned int peakToPeak0 = 0;   // peak-to-peak level
-  unsigned int peakToPeak1 = 0;   // peak-to-peak level
-  unsigned int peakToPeak2 = 0;   // peak-to-peak level
-  unsigned int peakToPeak3 = 0;   // peak-to-peak level
-  unsigned int peakToPeak4 = 0;   // peak-to-peak level
+  unsigned int peakToPeak[5] = {0, 0, 0, 0, 0};   // peak-to-peak level
 
-  unsigned int signalMax0 = 0;
-  unsigned int signalMax1 = 0;
-  unsigned int signalMax2 = 0;
-  unsigned int signalMax3 = 0;
-  unsigned int signalMax4 = 0;
-  unsigned int signalMin0 = 1024;
-  unsigned int signalMin1 = 1024;
-  unsigned int signalMin2 = 1024;
-  unsigned int signalMin3 = 1024;
-  unsigned int signalMin4 = 1024;
+
+  unsigned int signalMax[5] = {0, 0, 0, 0, 0};
+
+  unsigned int signalMin[5] = {1024, 1024, 1024, 1024, 1024};
+
 
   // collect data for 50 mS and then plot data
   while (millis() - startMillis < sampleWindow)
   {
-    sample0 = analogRead(pin0);
-    sample1 = analogRead(pin1);
-    sample2 = analogRead(pin2);
-    sample3 = analogRead(pin3);
-    sample4 = analogRead(pin4);
-    if (sample0 < 1024)  // toss out spurious readings
-    {
-      if (sample0 > signalMax0)
-      {
-        signalMax0 = sample0;  // save just the max levels
-      }
-      else if (sample0 < signalMin0)
-      {
-        signalMin0 = sample0;  // save just the min levels
-      }
-    }
-    if (sample1 < 1024)  // toss out spurious readings
-    {
-      if (sample1 > signalMax1)
-      {
-        signalMax1 = sample1;  // save just the max levels
-      }
-      else if (sample1 < signalMin1)
-      {
-        signalMin1 = sample1;  // save just the min levels
-      }
-    }
-    if (sample2 < 1024)  // toss out spurious readings
-    {
-      if (sample2 > signalMax2)
-      {
-        signalMax2 = sample2;  // save just the max levels
-      }
-      else if (sample2 < signalMin2)
-      {
-        signalMin2 = sample2;  // save just the min levels
-      }
-    }
-    if (sample3 < 1024)  // toss out spurious readings
-    {
-      if (sample3 > signalMax3)
-      {
-        signalMax3 = sample3;  // save just the max levels
-      }
-      else if (sample3 < signalMin3)
-      {
-        signalMin3 = sample3;  // save just the min levels
-      }
-    }
-    if (sample4 < 1024)  // toss out spurious readings
-    {
-      if (sample4 > signalMax4)
-      {
-        signalMax4 = sample4;  // save just the max levels
-      }
-      else if (sample4 < signalMin4)
-      {
-        signalMin4 = sample4;  // save just the min levels
+    for (int i=0; i<5; i++) {
+      sample[i] = analogRead(pins[i]);
+
+
+      if (sample[i] < 1024) {
+        if (sample[i] > signalMax[i]) {
+          signalMax[i] = sample[i];
+        } else if (sample[i] < signalMin[i]) {
+          signalMin[i] = sample[i];
+        }
       }
     }
   }
-  peakToPeak0 = signalMax0 - signalMin0;  // max - min = peak-peak amplitude
-  Serial.print(peakToPeak0);
-  Serial.print("  ");
-  peakToPeak1 = signalMax1 - signalMin1;  // max - min = peak-peak amplitude
-  Serial.print(peakToPeak1);
-  Serial.print("  ");
-  peakToPeak2 = signalMax2 - signalMin2;  // max - min = peak-peak amplitude
-  Serial.print(peakToPeak2);
-  Serial.print("  ");
-  peakToPeak3 = signalMax3 - signalMin3;  // max - min = peak-peak amplitude
-  Serial.print(peakToPeak3);
-  Serial.print("  ");
-  peakToPeak4 = signalMax4 - signalMin4;  // max - min = peak-peak amplitude
-  Serial.print(peakToPeak4);
-  Serial.print("  ");
-  //double volts = (peakToPeak * 5.0) / 1024;  // convert to volts
-  //Serial.println(volts);
 
-    // put your main code here, to run repeatedly:
-  // char x = SUART.read(); 
-  // if (SUART.available()){
-  //   if (x != 0x0A)  //end mark no found
-  //   {
-  //     myData[i] = x;  //save ASCII coded data in array
-  //     i++;
-  //   }
-  //   else
-  //   {
-  //     z = atoi(myData);  		 // getting the data in integer form
-  //     i = 0;
-  //     Serial.println("end");
-  //     while (f=0,f<=55,f++){
-  //       myData[f]="";
-  //     }
-  //   }
-  // // Serial.println(z);
-  // Serial.println(myData);
-  // Serial.print(SUART.available());
+  myString = "";
+  for (int i=0; i<5; i++) {
+    peakToPeak[i] = signalMax[i] - signalMin[i];  // max - min = peak-peak amplitude
+    myString+= String(peakToPeak[i]);
+    myString+= "  ";
+  }
   
   SUART.listen(); // listening on Serial One
 
-  // Serial.println("Data from port one:");
+  Serial.print(myString);
   while (SUART.available() > 0) {
     char inByte = SUART.read();
     Serial.write(inByte);
   }
   Serial.println();
-  
 }
+
--- src/transmitter/transmitter.ino
+++ src/transmitter/transmitter.ino
@@ -5,11 +5,9 @@ int const pin2 = A2;       // Preamp output pin connected to A0
 int const pin3 = A3;       // Preamp output pin connected to A0
 int const pin4 = A4;       // Preamp output pin connected to A0
 
-unsigned int sample0;
-unsigned int sample1;
-unsigned int sample2;
-unsigned int sample3;
-unsigned int sample4;
+int const pins[5] = {pin0, pin1, pin2, pin3, pin4};
+unsigned int sample[5] = {0, 0, 0, 0, 0};
+
 
 bool transmitting = true;
 
@@ -20,9 +18,7 @@ String myString = "";
 #include<SoftwareSerial.h>  // The library to create a secondary serial monitor on arduino uno.
 
 SoftwareSerial SUART(2, 3); // Sets the input and output ports to Digital Pins 3 and 4. They should be reversed with the pins on the speedometer. 
-char myData[10] = "";       // Creates a blank character array of size 10
 int i = 0;
-int z;
 
 
 
@@ -36,109 +32,43 @@ void setup()
 void loop()
 {
   unsigned long startMillis = millis(); // Start of sample window
-  unsigned int peakToPeak0 = 0;   // peak-to-peak level
-  unsigned int peakToPeak1 = 0;   // peak-to-peak level
-  unsigned int peakToPeak2 = 0;   // peak-to-peak level
-  unsigned int peakToPeak3 = 0;   // peak-to-peak level
-  unsigned int peakToPeak4 = 0;   // peak-to-peak level
+  unsigned int peakToPeak[5] = {0, 0, 0, 0, 0};   // peak-to-peak level
 
-  unsigned int signalMax0 = 0;
-  unsigned int signalMax1 = 0;
-  unsigned int signalMax2 = 0;
-  unsigned int signalMax3 = 0;
-  unsigned int signalMax4 = 0;
-  unsigned int signalMin0 = 1024;
-  unsigned int signalMin1 = 1024;
-  unsigned int signalMin2 = 1024;
-  unsigned int signalMin3 = 1024;
-  unsigned int signalMin4 = 1024;
+
+  unsigned int signalMax[5] = {0, 0, 0, 0, 0};
+
+  unsigned int signalMin[5] = {1024, 1024, 1024, 1024, 1024};
+
 
   // collect data for 50 mS and then plot data
   while (millis() - startMillis < sampleWindow)
   {
-    sample0 = analogRead(pin0);
-    sample1 = analogRead(pin1);
-    sample2 = analogRead(pin2);
-    sample3 = analogRead(pin3);
-    sample4 = analogRead(pin4);
-    if (sample0 < 1024)  // toss out spurious readings
-    {
-      if (sample0 > signalMax0)
-      {
-        signalMax0 = sample0;  // save just the max levels
-      }
-      else if (sample0 < signalMin0)
-      {
-        signalMin0 = sample0;  // save just the min levels
-      }
-    }
-    if (sample1 < 1024)  // toss out spurious readings
-    {
-      if (sample1 > signalMax1)
-      {
-        signalMax1 = sample1;  // save just the max levels
-      }
-      else if (sample1 < signalMin1)
-      {
-        signalMin1 = sample1;  // save just the min levels
-      }
-    }
-    if (sample2 < 1024)  // toss out spurious readings
-    {
-      if (sample2 > signalMax2)
-      {
-        signalMax2 = sample2;  // save just the max levels
-      }
-      else if (sample2 < signalMin2)
-      {
-        signalMin2 = sample2;  // save just the min levels
-      }
-    }
-    if (sample3 < 1024)  // toss out spurious readings
-    {
-      if (sample3 > signalMax3)
-      {
-        signalMax3 = sample3;  // save just the max levels
-      }
-      else if (sample3 < signalMin3)
-      {
-        signalMin3 = sample3;  // save just the min levels
-      }
-    }
-    if (sample4 < 1024)  // toss out spurious readings
-    {
-      if (sample4 > signalMax4)
-      {
-        signalMax4 = sample4;  // save just the max levels
-      }
-      else if (sample4 < signalMin4)
-      {
-        signalMin4 = sample4;  // save just the min levels
+    for (i=0; i<5; i++) {
+      sample[i] = analogRead(pins[i]);
+
+
+      if (sample[i] < 1024) {
+        if (sample[i] > signalMax[i]) {
+          signalMax[i] = sample[i];
+        } else if (sample[i] < signalMin[i]) {
+          signalMin[i] = sample[i];
+        }
       }
     }
   }
-  peakToPeak0 = signalMax0 - signalMin0;  // max - min = peak-peak amplitude
-  peakToPeak1 = signalMax1 - signalMin1;  // max - min = peak-peak amplitude
-  peakToPeak2 = signalMax2 - signalMin2;  // max - min = peak-peak amplitude
-  peakToPeak3 = signalMax3 - signalMin3;  // max - min = peak-peak amplitude
-  peakToPeak4 = signalMax4 - signalMin4;  // max - min = peak-peak amplitude
-  Serial.print(peakToPeak0);
-  Serial.print("   ");
-  Serial.print(peakToPeak1);
-  Serial.print("   ");
-  Serial.print(peakToPeak2);
-  Serial.print("   ");
-  Serial.print(peakToPeak3);
-  Serial.print("   ");
-  Serial.println(peakToPeak4);
+  myString = "";
+  for (i=0; i<5; i++) {
+    peakToPeak[i] = signalMax[i] - signalMin[i];  // max - min = peak-peak amplitude
+    myString+= String(peakToPeak[i]);
+    myString+= "  ";
+  }
+  
   //double volts = (peakToPeak * 5.0) / 1024;  // convert to volts
   //Serial.println(volts);
 
   // Serial.println(data);//write(Data);
   if (transmitting) {
-    myString = "";
-    myString+= String(peakToPeak0);
-    myString+= "  ";
-    
+    SUART.print(myString);
   }
+  Serial.println(myString);
 }
