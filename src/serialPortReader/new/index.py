48c23e45398a1a8f7adc85e63671697edfb17bfc
Theawesometurtl <shablammmo@gmail.com>
2024-03-11 14:44:21 -0700

    idk

--- src/serialPortReader/new/index.py
+++ src/serialPortReader/new/index.py
@@ -1,6 +1,7 @@
 # Python code transmits a byte to Arduino /Microcontroller
 import serial
 import time
+from matplotlib import pyplot as plt
 SerialObj = serial.Serial('COM5') # COMxx  format on Windows
                   # ttyUSBx format on Linux
 SerialObj.baudrate = 10000  # set Baud rate to 9600
@@ -17,8 +18,12 @@ while True:
     ReceivedString = ReceivedString.replace("\n","")
 
     ReceivedString = ReceivedString.split(" ")
+    spacing = []
     for i in range(0,6):
         ReceivedString[i] = int(ReceivedString[i])
         print(ReceivedString[i])
     print(ReceivedString[6])
+    plt.plot(ReceivedString)
+    plt.legend() 
+    plt.show()
 SerialObj.close() 
